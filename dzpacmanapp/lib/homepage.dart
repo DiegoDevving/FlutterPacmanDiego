fb75e13b1375e92a2ae039a04e4e70e91c499bc1
DiegoDev <diego.zavaleta4002@gmail.com>
2025-03-12 07:47:49 -0600

    Game Logic pt.1

--- dzpacmanapp/lib/homepage.dart
+++ dzpacmanapp/lib/homepage.dart
@@ -16,24 +16,139 @@ class _HomePageState extends State<HomePage> {
   int player = numberInRow * 15 + 1;
 
   List<int> barriers = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 21,
-    22, 24, 26, 28, 30, 32,
-    33, 35, 37, 38, 39, 41, 43,
-    44, 46, 52, 54,
-    55, 57, 59, 61, 63, 65,
-    66, 70, 72, 76,
-    77, 78, 79, 80, 81, 83, 84, 85, 86, 87,
-    99, 100, 101, 102, 103, 105, 106, 107, 108, 109,
-    110, 114, 115, 116, 120,
-    121, 123, 125, 127, 129, 131,
-    132, 134, 140, 142,
-    143, 145, 147, 148, 149, 151, 153,
-    154, 156, 158, 160, 162, 164,
-    165, 175,
-    176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    21,
+    22,
+    24,
+    26,
+    28,
+    30,
+    32,
+    33,
+    35,
+    37,
+    38,
+    39,
+    41,
+    43,
+    44,
+    46,
+    52,
+    54,
+    55,
+    57,
+    59,
+    61,
+    63,
+    65,
+    66,
+    70,
+    72,
+    76,
+    77,
+    78,
+    79,
+    80,
+    81,
+    83,
+    84,
+    85,
+    86,
+    87,
+    99,
+    100,
+    101,
+    102,
+    103,
+    105,
+    106,
+    107,
+    108,
+    109,
+    110,
+    114,
+    115,
+    116,
+    120,
+    121,
+    123,
+    125,
+    127,
+    129,
+    131,
+    132,
+    134,
+    140,
+    142,
+    143,
+    145,
+    147,
+    148,
+    149,
+    151,
+    153,
+    154,
+    156,
+    158,
+    160,
+    162,
+    164,
+    165,
+    175,
+    176,
+    177,
+    178,
+    179,
+    180,
+    181,
+    182,
+    183,
+    184,
+    185,
+    186,
   ];
+  String direction = "right";
 
+  void startGame() {
+    Timer.periodic(Duration(milliseconds: 150), (timer) {
+      switch (direction) {
+        case "left":
+        moveLeft();
+          break;
+        case "right":
+        moveRight();
+          break;
+        case "up":
+        moveUp();
+          break;
+        case "down":
+        moveDown();
+          break;
+        default:
+      }
+      if (!barriers.contains(player + 1)) {
+        setState(() {
+          player++;
+        });
+      }
+    });
+  }
+  
+  void moveLeft(){}
+  void moveRight(){}
+  void moveUp(){}
+  void moveDown(){}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,32 +157,50 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(
             flex: 5,
-            child: Container(
-              child: GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: numberOfSquares,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: numberInRow,
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.delta.dy > 0) {
+                  direction = "up";
+                } else if (details.delta.dy < 0) {
+                  direction = "down";
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0) {
+                  direction = "right";
+                } else if (details.delta.dx < 0) {
+                  direction = "left";
+                }
+              },
+              child: Container(
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: numberInRow,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (player == index) {
+                      return MyPlayer();
+                    } else if (barriers.contains(index)) {
+                      return MyPixel(
+                        innerColor:
+                            Colors
+                                .blue[800], // Choose the color you want for barriers
+                        outerColor: Colors.blue[900],
+                        //child: Text(index.toString()),
+                      );
+                    } else {
+                      return MyPath(
+                        innerColor:
+                            Colors
+                                .yellow, // Choose the color you want for barriers
+                        outerColor: Colors.black,
+                        //child: Text(index.toString()),
+                      );
+                    }
+                  },
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  if (player == index) {
-                    return MyPlayer(
-
-                    );
-                  }else if (barriers.contains(index)) {
-                    return MyPixel(
-                      innerColor: Colors.blue[800], // Choose the color you want for barriers
-                      outerColor: Colors.blue[900],
-                      //child: Text(index.toString()),
-                    );
-                  } else {
-                    return MyPath(
-                      innerColor: Colors.yellow, // Choose the color you want for barriers
-                      outerColor: Colors.black,
-                      //child: Text(index.toString()),
-                    );
-                  }
-                },
               ),
             ),
           ),
@@ -75,11 +208,18 @@ class _HomePageState extends State<HomePage> {
             child: Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text("Score: ",
-                      style: TextStyle(color: Colors.white, fontSize: 40)),
-                  Text("P L A Y",
-                      style: TextStyle(color: Colors.white, fontSize: 40)),
+                children: [
+                  const Text(
+                    "Score: ",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  GestureDetector(
+                    onTap: StartGame,
+                    child: Text(
+                      "P L A Y",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
                 ],
               ),
             ),
